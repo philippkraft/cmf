bd585ac90bb4bb0cf2d628113aefa451f3c3e0cb
Philipp Kraft <philipp.kraft@umwelt.uni-giessen.de>
2009-11-05 14:04:32 +0000

    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/trunk@23 a735f7ef-1525-764e-8607-acac38503800

---
+++ pyCMF/src/Check_Manning_Diffusive.py
@@ -0,0 +1,15 @@
+'''
+Created on 05.11.2009
+
+@author: philkraf
+'''
+import cmf._cmf_core
+p=cmf.project()
+R1 = p.NewReach(cmf.Channel('R',1,1),1)
+R2 = p.NewReach(cmf.Channel('R',1,1),1)
+R1.set_downstream(R2)         
+Rs=cmf.node_list.from_sequence((R1,R2))
+R1.depth=0.5
+R2.depth=0.1
+integ=cmf.ImplicitEuler(Rs,1e-6)
+integ(integ.t+cmf.min) 
