835be5de58b456b14da2ffe207cb12f038326f57
Philipp Kraft <philipp.kraft@umwelt.uni-giessen.de>
2009-11-16 09:47:18 +0000

    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/new_order@39 a735f7ef-1525-764e-8607-acac38503800

--- cmf_setups/rice_paddies.py
+++ cmf_setups/rice_paddies.py
@@ -5,6 +5,7 @@ Created on Thu Nov 05 09:10:18 2009
 @author: philkraf
 """
 import cmf
+from DECOMP.cmf_DECOMP import DECOMPcmf
 from matplotlib.pylab import *
 def add_cell(p,x,h,last_cell):
     for i in range(1):
@@ -65,14 +66,14 @@ def make_rice_paddies(project,count=3,cells_per_paddy=10,dam_height=1.0,terrace_
     #project[-1].connect_soil_with_node(dbc,cmf.Richards_lateral,10,1)
     return pools,dbc, pool_of_cell
 
-def draw_fig(storages,t=cmf.Time(),qscale=100,alpha=1.0):
+def draw_fig(storages,t=cmf.Time(),qscale=100,alpha=1.0,tracer=None):
     ioff()
     r=cmf.Raster(shape=(max(c.layer_count() for c in p),len(p)),
                  corner=(-0.5,0),cellsize=(1,0.1))
     for s in storages:
         if isinstance(s, cmf.SoilLayer):
-            r[s.Location.x, s.Location.z] = s.wetness
-    r.draw(hold=0,cmap=cm.RdYlBu,alpha=alpha,vmax=1.0,zorder=1,interpolation='bilinear')
+            r[s.Location.x, s.Location.z] = s.conc(tracer) if tracer else s.wetness 
+    r.draw(hold=0,cmap=cm.RdYlGn_r if tracer else cm.RdYlBu,alpha=alpha,vmax=1.0,zorder=1,interpolation='nearest')
     axis('tight')
     pos=storages.get_positions()
     f=storages.get_fluxes3d(t)
@@ -82,7 +83,7 @@ def draw_fig(storages,t=cmf.Time(),qscale=100,alpha=1.0):
     draw()
     ion()
 if __name__=='__main__':
-    p=cmf.project()
+    p=cmf.project("N DOC")
     pool_length=20
     dam_height=0.5
     terrace_height=0.5
@@ -93,9 +94,12 @@ if __name__=='__main__':
     storages=cmf.node_list.from_sequence(pools)
     storages.extend(cmf.get_layers(p))
     for c in pool_cell_dict:
-        c.layers[0].soil.SetKsat(0.1,0.0)
-    integ=cmf.CVodeIntegrator(storages,1e-6)
-    integ.preconditioner='R'
+        c.layers[2].soil.SetKsat(0.1,0.0)
+        c.layers[2].soil.Set_b(7.5)
+    winteg=cmf.CVodeIntegrator(1e-6)
+    winteg.preconditioner='R'
+    sinteg=cmf.BDF2(1e-6)
+    integ=cmf.SoluteWaterIntegrator(winteg,sinteg,p)    
     pools[0].depth = dam_height
     b = lambda : bar(left  =[(pool_length+2) * i +.5 for i in range(pool_count) ],
                      bottom=[(pool_count-i+2)*terrace_height for i in range(pool_count)],
@@ -103,22 +107,29 @@ if __name__=='__main__':
                      height=[pool.depth for pool in pools],
                      zorder=0,fc=(0.6,0.6,1.0),ec=(0.3,0.3,1.0))
     scale=500
-    def run(for_t=cmf.min*15):
+    
+    DECOMPcells=dict((c,DECOMPcmf(c)) for c in pool_cell_dict)
+    for c,D in DECOMPcells.iteritems():
+        D.DECOMPlayers[0] = DECOMP.SOM(0.1*1e2,1e2)
+
+    def run(p,for_t=cmf.min*15):
         for c,P in pool_cell_dict.iteritems():
             c.surfacewater.potential = P.potential
             c.surfacewater.is_source = not P.is_empty()
+            DECOMPcells[c].run(27,for_t.AsDays())
         integ(integ.t+for_t)
         for c,P in pool_cell_dict.iteritems():
             P.volume += c.surfacewater.water_balance(integ.t) * for_t.AsDays()
+        subplot(212)
+        draw_fig(storages,integ.t,scale,0.85,tracer=p.solutes[0])
+        b()
+        subplot(211)
         draw_fig(storages,integ.t,scale,0.85)
         b()
         title(integ.t)
         return integ.t, [p.depth for p in pools]
-    for i in range(24*14):
-        run()
-        savefig('rice_film/rice%05i.png' % i)
-        if pools[0].depth<0.05:
-            pools[0].depth=dam_height
+    N,DOC=p.solutes
+    show()
     
     
  
