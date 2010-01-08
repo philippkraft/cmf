78f940341c7339096180a32444be2b8640a124cc
no_author <no_author@no_author>
2010-01-08 14:42:41 +0000

    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/trunk@63 a735f7ef-1525-764e-8607-acac38503800

--- setup.py
+++ setup.py
@@ -1,35 +1,12 @@
 import os
-## from numpy.distutils.core import setup
-## from numpy.distutils.misc_util import Configuration
-## def make_cmf_core(parent_package='',top_path='.'):
-##     cmf_core = Configuration('cmf_core', parent_package,top_path)
-##     library_dirs=["../../OpenSource/cvode-2.5.0/gcc"]
-##     include_dirs=["../../OpenSource/cvode-2.5.0/include"]
-##     libraries=["cvode"]
-##     cmf_files=[]
-##     for root,dirs,files in os.walk(top_path):
-##         cmf_files.extend(os.path.join(root,f) for f in files if f.endswith('.cpp'))
-##     print "Compiling %i source files" % (len(cmf_files)+1)
-##     cmf_files.append("cmf_wrap.cxx")
-##     cmf_core.add_extension("_cmf_core",
-##         sources=cmf_files,
-##         library_dirs=library_dirs,
-##         libraries = libraries,
-##         include_dirs=include_dirs,
-##         extra_compile_args=["-fopenmp"],
-##         extra_link_args=["-fopenmp"],
-##     )
-##     return cmf_core
-## if __name__ == '__main__':
-##     cmf_core = make_cmf_core()
-##     setup(**cmf_core.todict())
 
 from distutils.core import setup,Extension
-    
+
 def make_cmf_core():
-    library_dirs=["../../OpenSource/cvode-2.5.0/gcc"]
-    include_dirs=["../../OpenSource/cvode-2.5.0/include"]
-    libraries=["cvode"]
+    library_dirs=["../sundials-2.4.0/instdir/lib"]
+    include_dirs=["../sundials-2.4.0/instdir/include",
+                  "../boost_1_41_0","../boost_1_41_0/boost/tr1"]
+    libraries=["sundials_cvode","sundials_nvecserial"]
     cmf_files=[]
     for root,dirs,files in os.walk('.'):
         cmf_files.extend(os.path.join(root,f) for f in files if f.endswith('.cpp'))
