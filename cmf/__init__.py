dbb98919c82e0cbd5e90ad7615b5d8a8bbd6c801
no_author <no_author@no_author>
2010-01-15 12:53:56 +0000

    Added Copyright notice and GPL v2
    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/trunk@72 a735f7ef-1525-764e-8607-acac38503800

--- cmf/__init__.py
+++ cmf/__init__.py
@@ -1,3 +1,21 @@
+
+
+# Copyright 2010 by Philipp Kraft
+# This file is part of cmf.
+#
+#   cmf is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   cmf is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
+#   
 from cmf_core import *
 from cell_factory import create_reaches,cells_from_dem,cells_from_polygons,create_reaches_preintersect,geometry
 from geos_shapereader import shapefile
