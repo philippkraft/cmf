26e2e7409746e22a248ffc96ff562eb62d177865
nobody <16303297+philippkraft@users.noreply.github.com>
2010-01-15 12:53:56 +0000

    Added Copyright notice and GPL v2

--- cmf/cmf_core_src/cmf.cpp
+++ cmf/cmf_core_src/cmf.cpp
@@ -1,5 +1,23 @@
 
 
+// Copyright 2010 by Philipp Kraft
+// This file is part of cmf.
+//
+//   cmf is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   cmf is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
+//   
+
+
 #ifdef _CONSOLE
 	
 	// Include a little bit of everything for everybody
--- cmf/soil.py
+++ cmf/soil.py
@@ -1,4 +1,22 @@
 # coding=utf-8
+
+
+# Copyright 2010 by Philipp Kraft
+# This file is part of cmf.
+#
+#   cmf is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   cmf is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
+#   
 """ This module provides a soil class for the retrieval of relevant data for hydrologic modeling
 The basic concept is a soilType->Layer relationship. Most of the data is stored in the layer class
 If you'd like to use mean values for the whole soil profile, depth weighted means for any layer method are available
