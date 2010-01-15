9cccf2090f07ad4fbb2c2441999c9ac455c364f6
no_author <no_author@no_author>
2010-01-15 12:04:04 +0000

    Version for EMS submitted paper
    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/trunk@69 a735f7ef-1525-764e-8607-acac38503800

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+#! /usr/bin/env python
+
+# This file can build and install cmf
+
+
+# Change these pathes to your sundials 2.4+ installation
+sundials_lib_path = r"D:\Code\sundials-2.4.0\inst_msvc\lib"
+sundials_include_path = r"D:\Code\sundials-2.4.0\inst_msvc\include"
+
+# Change this path to your boost installation (not needed for gcc)
+boost_path = r"D:\Code\boost_1_41_0"
+
+# Change these variables to match your compiler. (gcc or 
+# For Visual Studio 2008 no action is needed
+gcc = 0
+msvc = 1
+
+
+# No user action required beyond this point
 import os
 
 from distutils.core import setup,Extension
 
 def make_cmf_core():
-    library_dirs=[r"D:\Code\sundials-2.4.0\instdir\lib"]
-    include_dirs=[r"D:\Code\sundials-2.4.0\instdir\include",
-                  r"D:\Code\boost_1_41_0",r"D:\Code\boost_1_41_0\boost\tr1"]
+    library_dirs=[sundials_lib_path]
+    include_dirs=[sundials_include_path]
+    include_dirs += [boost_path,boost_path+r"\boost\tr1"]
+    if msvc: compile_args = ["/openmp","/EHsc"]
+    if gcc: compile_args = ["-std=gnu++98","-fopenmp","-w"]
     libraries=["sundials_cvode","sundials_nvecserial"]
     cmf_files=[]
     for root,dirs,files in os.walk('.'):
@@ -17,23 +38,34 @@ def make_cmf_core():
                             library_dirs=library_dirs,
                             libraries = libraries,
                             include_dirs=include_dirs,
-                            extra_compile_args=["/openmp","/EHsc"],
+                            extra_compile_args=compile_args,
                         )
     return cmf_core
 def make_raster():
     files=[r'cmf\raster\raster_src\raster_wrap.cxx']
+    if msvc: compile_args = ["/openmp","/EHsc"]
+    if gcc: compile_args = ["-std=gnu++98","-fopenmp","-w"]
     raster = Extension('cmf.raster._raster',
                         sources=files,
-                        extra_compile_args=["/openmp"],
+                        extra_compile_args=compile_args,
                     )
     return raster
 if __name__=='__main__':
-    ext = [make_cmf_core(),make_raster()]
+    ext = [make_raster(),make_cmf_core()]
     author = "Philipp Kraft"
     author_email = "philipp.kraft@umwelt.uni-giessen.de"
     url = "www.uni-giessen.de/ilr/frede/cmf"
     py=[]
     for root,dirs,files in os.walk('cmf'):
         py.extend(os.path.join(root,f[:-3]).replace('\\','.') for f in files if f.endswith('.py'))
-    setup(name='cmf',version='0.8',ext_modules=ext,py_modules=py)
+    
+    
+    setup(name='cmf',
+          version='0.1',
+          license='GPL',
+          ext_modules=ext,
+          py_modules=py, 
+          requires=['shapely (>=1.0)'],
+          data_files=[('help',['cmf.chm'])])
+    
     print "build ok"
