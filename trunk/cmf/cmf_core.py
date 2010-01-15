e16589d63cca2e4e9555c5550f6b43ca93dce045
nobody <nobody@a735f7ef-1525-764e-8607-acac38503800>
2010-01-15 12:53:56 +0000

    Added Copyright notice and GPL v2

--- trunk/cmf/cmf_core.py
+++ trunk/cmf/cmf_core.py
@@ -4,6 +4,24 @@
 # Do not make changes to this file unless you know what you are doing--modify
 # the SWIG interface file instead.
 
+
+# Copyright 2010 by Philipp Kraft
+# This file is part of cmf.
+#
+#   cmf is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   cmf is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
+#   
+
 from sys import version_info
 if version_info >= (3,0,0):
     new_instancemethod = lambda func, inst, cls: _cmf_core.SWIG_PyInstanceMethod_New(func)
--- trunk/cmf/cmf_core_src/water/SoluteStorage.h
+++ trunk/cmf/cmf_core_src/water/SoluteStorage.h
@@ -1,3 +1,21 @@
+
+
+// Copyright 2010 by Philipp Kraft
+// This file is part of cmf.
+//
+//   cmf is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 2 of the License, or
+//   (at your option) any later version.
+//
+//   cmf is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
+//   
 #ifndef SoluteStorage_h__
 #define SoluteStorage_h__
 
