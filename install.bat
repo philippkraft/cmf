6bddc338ea7da4e98bf6f4fcc471462fc91f7b62
no_author <no_author@no_author>
2010-05-05 12:24:47 +0000

    Cells from a dem a can be created using different direction_counts
    git-svn-id: svn://fb09-pasig.umwelt.uni-giessen.de/cmf/trunk@146 a735f7ef-1525-764e-8607-acac38503800

--- install.bat
+++ install.bat
@@ -1,4 +1,10 @@
-python setup.py build_ext
+if not "x%1x"=="xdoxyx" goto build
+doxygen Doxyfile
+python doxy2swig.py doxy\xml\index.xml cmf\cmf_core_src\docstrings.i
+ren cmf\cmf_core_src\cmf.i cmf_1.i
+ren cmf\cmf_core_src\cmf_1.i cmf.i
+:build
+python setup.py build_ext 
 move /Y cmf_core.py cmf\
 move /Y raster.py cmf\raster\
 python setup.py build_py -c -O2 -f
