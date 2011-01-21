c50317a56d93248afa18312bf775f6a3f930bbd0
philipp <philipp@a735f7ef-1525-764e-8607-acac38503800>
2011-01-21 09:20:16 +0000

    Some new documentation and fixing of problem with overloaded MeteoStationList.add_station

--- trunk/cmf/cmf_core_src/upslope/conductable.h
+++ trunk/cmf/cmf_core_src/upslope/conductable.h
@@ -7,7 +7,7 @@
 namespace cmf {
 	namespace upslope {
 		/// An abstract interface for all classes providing a conductivity (e.g. soil layer or aquifer)
-		/// Each water storage were the concept of conductivity is applicable should implement (by deriving) the interface
+		/// Each water storage where the concept of conductivity is applicable should implement (by deriving) the interface
 		///
 		/// This class is not part of the Python interface, since a dynamic typed language can use duck typing instead of this interface
 		class conductable {
