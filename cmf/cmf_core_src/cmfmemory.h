9955d8f0d2d582df1e25ebd6c7e45c0266371be1
philipp <16303297+philippkraft@users.noreply.github.com>
2017-09-05 07:47:42 +0000

    Removed superfluous files added from Ina long time ago.

--- cmf/cmf_core_src/cmfmemory.h
+++ cmf/cmf_core_src/cmfmemory.h
@@ -4,16 +4,17 @@
 #if __cplusplus > 199711L || _MSC_VER > 1800 || SWIG
 #pragma message("using std::shared_ptr")
 #include <memory>
-#elif __GNUG__ >= 4 && __GNUC_MINOR__ >=3 && __GNUC_MINOR__ < 8
+#elif __GNUG__ >= 4 && __GNUC_MINOR__ >=3
 #pragma message("using std::tr1::shared_ptr")
 #include <tr1/memory>
-
+#include <tr1/cmath>
 namespace std {
 	using std::tr1::shared_ptr;
 	using std::tr1::weak_ptr;
 	using std::tr1::enable_shared_from_this;
 	using std::tr1::dynamic_pointer_cast;
 	using std::tr1::static_pointer_cast;
+	using std::tr1::isfinite
 }
 
 #else
@@ -22,6 +23,7 @@ namespace std {
 #include <boost/pointer_cast.hpp>
 #include <boost/weak_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+
 namespace std
 {
 	using boost::shared_ptr;
@@ -29,6 +31,9 @@ namespace std
 	using boost::enable_shared_from_this;
 	using boost::dynamic_pointer_cast;
 	using boost::static_pointer_cast;
+	inline bool isfinite(double v) {
+		return v == v;
+	}
 }
 #endif
 
