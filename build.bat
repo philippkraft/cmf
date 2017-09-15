6aee267f90c1da09286ad8a091c594cb7bbaa1a1
philipp <16303297+philippkraft@users.noreply.github.com>
2017-09-15 13:57:56 +0000

    New versioning scheme

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+echo delete old .pyd
+del _cmf_core*.pyd /s /q
+echo build cmf
+python setup.py build_ext swig
+if ERRORLEVEL 1 goto error
+move /Y cmf\cmf_core_src\cmf_core.py cmf
+python setup.py build_py -c -O2 -f
+if ERRORLEVEL 1 goto error
+goto end
+:error
+echo ************************
+echo Installation had error !
+echo ************************
+EXIT /B 1
+:end
+echo .
