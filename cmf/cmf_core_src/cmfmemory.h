06dfba60fb894803b34adeb9686d311248d79b1b
Philipp Kraft <oekopez@gmx.de>
2017-12-14 15:46:10 +0100

    Removed tr1 support from namespace. Now only std c++11 and boost smart pointers are supported.

--- cmf/cmf_core_src/cmfmemory.h
+++ cmf/cmf_core_src/cmfmemory.h
@@ -2,23 +2,11 @@
 #define cmfmemory_h__
 
 #if __cplusplus > 199711L || _MSC_VER > 1800 || SWIG
-#pragma message("using std::shared_ptr")
+
 #include <memory>
-#elif __GNUG__ >= 4 && __GNUC_MINOR__ >=3
-#pragma message("using std::tr1::shared_ptr")
-#include <tr1/memory>
-#include <tr1/cmath>
-namespace std {
-	using std::tr1::shared_ptr;
-	using std::tr1::weak_ptr;
-	using std::tr1::enable_shared_from_this;
-	using std::tr1::dynamic_pointer_cast;
-	using std::tr1::static_pointer_cast;
-	using std::tr1::isfinite
-}
 
 #else
-#pragma message("using boost::shared_ptr")
+
 #include <boost/shared_ptr.hpp>
 #include <boost/pointer_cast.hpp>
 #include <boost/weak_ptr.hpp>
