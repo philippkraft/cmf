bd5b29f9ae81cc3b47995f63e1ebc60ad0d8f073
bees4ever <beesforever@gmx.de>
2018-01-16 11:32:20 +0100

    Again a try to fix merge troubles based on git for file "ManningConnection.h"

--- cmf/cmf_core_src/reach/ManningConnection.h
+++ cmf/cmf_core_src/reach/ManningConnection.h
@@ -1,5 +1,4 @@
 
-
 // Copyright 2010 by Philipp Kraft
 // This file is part of cmf.
 //
@@ -15,7 +14,7 @@
 //
 //   You should have received a copy of the GNU General Public License
 //   along with cmf.  If not, see <http://www.gnu.org/licenses/>.
-//   
+//
 #ifndef ManningConnection_h__
 #define ManningConnection_h__
 
@@ -29,18 +28,18 @@ namespace cmf {
 
 		/// @defgroup manning Surface water connections
 		/// @ingroup connections
-		
+
 		/// @ingroup manning
 		/// @brief Calculates the flux between two open water bodies, using Manning's equation
-        ///
+		///
 		///
 		/// This is the base class for a kinematic wave approach (topography driven)
 		/// and a diffusive wave approach (water table driven). The only difference
 		/// between both approaches is the calculation of the flux driving slope.
-		/// For the model, one of ManningKinematic or ManningDiffusive connection 
+		/// For the model, one of ManningKinematic or ManningDiffusive connection
 		/// is selected
 		///
-        /// \f{eqnarray*}
+		/// \f{eqnarray*}
 		/// q_{Manning}&=& A R^{\frac 2 3} \sqrt{\frac {\Delta_z} n} \\
 		/// A &=& \frac V l \mbox{, (Crosssectional area of the wetted crossection, Volume per length)} \\
 		/// R &=& \frac A {P(d)} \\
@@ -50,7 +49,7 @@ namespace cmf {
 		/// n&=&\mbox{Manning friction number}
 		/// \f}
 		///
-		/// For the kinematic wave the slope of the river bed is used as slope \f$\Delta_z = \frac{|z_1 - z_2\|}{l}\f$, 
+		/// For the kinematic wave the slope of the river bed is used as slope \f$\Delta_z = \frac{|z_1 - z_2\|}{l}\f$,
 		///
 		/// while for the diffusive wave the slope is calculated from the actual water head. \f$\Delta_z = \|\frac{h_1 - h_2}{l}\f$
 		class Manning : public cmf::water::flux_connection
@@ -58,7 +57,6 @@ namespace cmf {
 		protected:
 
 			virtual real get_slope(cmf::water::flux_node::ptr lnode, cmf::water::flux_node::ptr rnode, real d)=0;
-
 			std::weak_ptr<cmf::river::OpenWaterStorage> w1,w2;
 			virtual real calc_q(cmf::math::Time t);
 			void NewNodes()
@@ -67,9 +65,9 @@ namespace cmf {
 				w2=cmf::river::OpenWaterStorage::cast(right_node());
 			}
 			Manning(cmf::river::OpenWaterStorage::ptr left,cmf::water::flux_node::ptr right,
-				const cmf::river::IVolumeHeightFunction& reachtype,bool diffusive_wave)
-				: flux_connection(left,right,diffusive_wave ? "Manning (diff. wave)" : "Manning (kin. wave)"),	flux_geometry(reachtype), is_diffusive_wave(diffusive_wave) {
-					NewNodes();
+					const cmf::river::IVolumeHeightFunction& reachtype,bool diffusive_wave)
+					: flux_connection(left,right,diffusive_wave ? "Manning (diff. wave)" : "Manning (kin. wave)"),	flux_geometry(reachtype), is_diffusive_wave(diffusive_wave) {
+				NewNodes();
 			}
 
 		public:
@@ -78,12 +76,11 @@ namespace cmf {
 
 			typedef std::shared_ptr<Manning> ptr;
 
-            static void connect_cells(cmf::upslope::Cell& c1,cmf::upslope::Cell& c2,bool is_diffusive_wave);
 
 		};
-        
+
 		/// @ingroup manning
-		/// Connecting surface water bodies using a diffusive wave. 
+		/// Connecting surface water bodies using a diffusive wave.
 		/// This approach might not be numerical stable for deep water with small gradient
 		/// cmf for experimental reasons
 		/// \f{eqnarray*}
@@ -98,15 +95,10 @@ namespace cmf {
 
 		class Manning_Diffusive: public Manning
 		{
-		private:
-
 		protected:
 			virtual real get_slope(cmf::water::flux_node::ptr lnode, cmf::water::flux_node::ptr rnode, real d);
 		public:
-            static void connect_cells(cmf::upslope::Cell& c1,cmf::upslope::Cell& c2,ptrdiff_t dummy)
-            {		cmf::river::Manning::connect_cells(c1,c2,true);		}
-            typedef std::shared_ptr<Manning_Diffusive> ptr;
-			static const cmf::upslope::CellConnector cell_connector;
+			typedef std::shared_ptr<Manning_Diffusive> ptr;
 			/// A parameter to linearize the dependency of q on slope during levelling out
 			real linear_slope_width;
 			/// Creates a diffusive wave connection between to open water storages.
@@ -114,12 +106,12 @@ namespace cmf {
 			/// @param left,right The nodes to be connected by the diffusive wave. Left needs to be an open water storage
 			/// @param reachtype The channel geometry
 			Manning_Diffusive(cmf::river::OpenWaterStorage::ptr left,cmf::water::flux_node::ptr right,const cmf::river::IChannel& reachtype)
-				: Manning(left,right,reachtype,true), linear_slope_width(0.0)
+					: Manning(left,right,reachtype,true), linear_slope_width(0.0)
 			{			}
 		};
 
 		/// @ingroup manning
-		/// Connecting surface water bodies using a kinematic wave. 
+		/// Connecting surface water bodies using a kinematic wave.
 		/// Note the fixed gradient \f$\Delta_z\f$
 		/// \f{eqnarray*}
 		/// q_{Manning}&=& A R^{\frac 2 3} \sqrt{\frac {\Delta_z} n} \\
@@ -131,24 +123,19 @@ namespace cmf {
         /// n&=&\mbox{Manning friction number}
 		/// \f}
 
-		
+
 		class Manning_Kinematic: public Manning
 		{
-		private:
-
 		protected:
 			virtual real get_slope(cmf::water::flux_node::ptr lnode, cmf::water::flux_node::ptr rnode, real d);
 		public:
-            static void connect_cells(cmf::upslope::Cell& c1,cmf::upslope::Cell& c2,ptrdiff_t dummy)
-            {		cmf::river::Manning::connect_cells(c1,c2,false);		}
 			typedef std::shared_ptr<Manning_Kinematic> ptr;
-			static const cmf::upslope::CellConnector cell_connector;
 			/// Creates a kinematic wave connection between to open water storages.
 			///
 			/// @param left,right The nodes to be connected by the kinematic wave. Left needs to be an open water storage
 			/// @param reachtype The channel geometry
 			Manning_Kinematic(cmf::river::OpenWaterStorage::ptr left,cmf::water::flux_node::ptr right,const cmf::river::IChannel& reachtype)
-				: Manning(left,right,reachtype,false)
+					: Manning(left,right,reachtype,false)
 			{			}
 		};
 		/*
@@ -163,7 +150,6 @@ namespace cmf {
 				w1=cmf::river::OpenWaterStorage::cast(left_node());
 				w2=cmf::river::OpenWaterStorage::cast(right_node());
 			}
-
 		public:
 			typedef std::shared_ptr<Manning> ptr;
 			static const cmf::upslope::CellConnector cell_connector;
@@ -172,7 +158,6 @@ namespace cmf {
 			{
 				NewNodes();
 			}
-
 		};
 		*/
 	}
