707103c05867860fb4ef6f12a07936983820ab28
Philipp Kraft <oekopez@gmx.de>
2018-01-17 16:51:07 +0100

    Added test for #14

---
+++ test/topology.py
@@ -0,0 +1,19 @@
+import cmf
+import unittest
+
+def get_setup():
+    p = cmf.project()
+    c = p.NewCell(0, 0, 0, 1000, True)
+    c2 = p.NewCell(0, 0, 0, 1000, True)
+    c.topology.AddNeighbor(c2, 1)
+    return p
+
+class TopologyTest(unittest.TestCase):
+    """
+    Test for regression on #14
+    """
+    def test_neighbor_iterator(self):
+        p = get_setup()
+        for n, w in p[0].neighbors:
+            self.assertIs(n, p[1])
+            self.assertEqual(w, 1)
