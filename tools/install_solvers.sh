184acb304111d48e0a8fea0ac24a1f0e3386672c
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2018-03-06 15:29:21 +0100

    Modernized solver installer for linux

--- tools/install_solvers.sh
+++ tools/install_solvers.sh
@@ -1,71 +1,79 @@
 #!/usr/bin/env bash
 
-# Gets and makes the dependencies for the new sparese CVodeIntegrator
-export KLUINSTALL_DIR=suitesparse-lib
-export SUNDIALS_DIR=sundials-lib
+CWD=$PWD
 
-# Test for BLAS
-if [[ $(ldconfig -p | grep blas) ]]; then
-    echo "BLAS ok"
-else
-    echo "Needs OpenBLAS, install on Ubuntu with:"
-    echo "sudo apt install libopenblas-dev"
-    echo "on HPC it is often there, load with module openblas"
-    exit 1
+TOOLSDIR=$CWD/$(dirname $0)
+
+# Gets and makes the dependencies for the new sparese CVodeIntegrator
+export CFLAGS="-fPIC"
+export KLUINSTALL_DIR=$TOOLSDIR/suitesparse-lib
+export SUNDIALS_DIR=$TOOLSDIR/sundials-lib
+
+function klu {
+    # Get KLU
+    git clone https://github.com/PetterS/SuiteSparse $TOOLSDIR/suitesparse
+
+    # Make KLU
+
+    mkdir -p ${KLUINSTALL_DIR}/lib
+    mkdir -p ${KLUINSTALL_DIR}/include
+    mkdir -p ${KLUINSTALL_DIR}/doc
+
+    cd $TOOLSDIR/suitesparse
+
+
+    make
+    make install \
+        INSTALL_LIB=${KLUINSTALL_DIR}/lib \
+        INSTALL_INCLUDE=${KLUINSTALL_DIR}/include \
+        INSTALL_DOC=${KLUINSTALL_DIR}/doc
+
+    cd $TOOLSDIR
+}
+
+function sundials {
+    # Get sundials
+    git clone https://github.com/philippkraft/sundials $TOOLSDIR/sundials
+
+    # Make sundials
+    mkdir -p ${SUNDIALS_DIR}
+
+    cd $TOOLSDIR/sundials
+
+    mkdir -p build
+    cd build
+
+    cmake .. \
+        -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
+        -DBLAS_ENABLE=ON \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DCMAKE_INSTALL_PREFIX=${SUNDIALS_DIR} \
+        -DEXAMPLES_INSTALL=ON \
+        -DKLU_ENABLE=ON \
+        -DKLU_LIBRARY_DIR=${KLUINSTALL_DIR}/lib \
+        -DKLU_INCLUDE_DIR=${KLUINSTALL_DIR}/include \
+        -DOPENMP_ENABLE=ON \
+
+    make
+    make install
+    
+    cd $TOOLSDIR
+}
+
+echo "Calling from: " $CWD
+echo "Running in: " $TOOLSDIR
+echo "Installing KLU into: " $KLUINSTALL_DIR
+echo "Installing SUNDIALS into: " $SUNDIALS_DIR
+
+if "$1" == "help"; then
+    exit 0
+fi
+    
+if "$1" != "sundials"; then
+    klu
 fi
 
-
-# Get KLU
-git clone https://github.com/PetterS/SuiteSparse suitesparse
-
-# Get sundials
-git clone https://github.com/philippkraft/sundials sundials
-
-
-
-# Make KLU
-
-# For of KLU & sundials build in Windows see:
-# http://my-it-notes.com/2013/01/how-to-build-suitesparse-under-windows-using-visual-studio/
-# https://github.com/jlblancoc/suitesparse-metis-for-windows
-# https://dmerej.info/blog/post/cmake-visual-studio-and-the-command-line/
-# https://ninja-build.org/
-
-
-
-mkdir -p ${KLUINSTALL_DIR}/lib
-mkdir -p ${KLUINSTALL_DIR}/include
-mkdir -p ${KLUINSTALL_DIR}/doc
-
-cd suitesparse
-
-
-make
-make install \
-    INSTALL_LIB=../${KLUINSTALL_DIR}/lib \
-    INSTALL_INCLUDE=../${KLUINSTALL_DIR}/include \
-    INSTALL_DOC=../${KLUINSTALL_DIR}/doc
-cd ..
-
-# Make sundials
-mkdir -p ${SUNDIALS_DIR}
-
-cd sundials
-
-mkdir -p build
-cd build
-
-cmake .. \
-    -DBLAS_ENABLE=ON \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_INSTALL_PREFIX=../../${SUNDIALS_DIR} \
-    -DEXAMPLES_INSTALL=OFF \
-    -DKLU_ENABLE=ON \
-    -DKLU_LIBRARY_DIR=../../${KLUINSTALL_DIR}/lib \
-    -DKLU_INCLUDE_DIR=../../${KLUINSTALL_DIR}/include \
-    -DOPENMP_ENABLE=ON \
-
-make
-make install
-
-cd ../..
+if "$1" != "klu"; then
+    sundials
+fi
+    
