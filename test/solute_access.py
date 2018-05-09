37eb074ebd9c2c7c93fec9d002c72e692ad1bc17
kraft-p <oekopez@gmx.de>
2018-05-09 14:19:50 +0200

    #29: Test bracket access functionality

---
+++ test/solute_access.py
@@ -0,0 +1,22 @@
+import cmf
+import unittest
+
+
+class TestSoluteAccess(unittest.TestCase):
+    def testConc(self):
+        p = cmf.project('X Y Z')
+        X, Y, Z = p.solutes
+        ws = p.NewStorage('storage')
+        self.assertEqual(repr(ws[X]), '{storage}[X]')
+        self.assertEqual(ws.conc(X), ws[X].conc)
+
+    def testState(self):
+        p = cmf.project('X Y Z')
+        X, Y, Z = p.solutes
+        ws = p.NewStorage('storage')
+        ws.volume = 1.0
+        ws[X].state = 1.0
+        ws.volume = 2.0
+        self.assertAlmostEquals(ws[X].conc, 0.5)
+        self.assertAlmostEquals(ws[X].state, 1.0)
+        self.assertAlmostEquals(ws[Y].conc, 0.0)
