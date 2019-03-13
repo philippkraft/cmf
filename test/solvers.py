d683220e684c3a3f6b395e32f104887e3a6fc300
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2019-03-13 14:31:57 +0100

    Added test for solver accuracy

--- test/solvers.py
+++ test/solvers.py
@@ -7,30 +7,45 @@ import numpy as np
 import unittest
 
 
-def get_project():
-    p = cmf.project('X')
-    X, = p.solutes
+def get_project(with_solute=False):
+    if with_solute:
+        p = cmf.project('X')
+        X, = p.solutes
+    else:
+        p = cmf.project()
+        X = None
     stores = [p.NewStorage('source{}'.format(i)) for i in range(10)]
+
     for l, r in zip(stores[:-1],stores[1:]):
         cmf.LinearStorageConnection(l, r, 1)
     stores[0].volume = 1
-    stores[0][X].source = 1
-    return p, stores
+    if with_solute:
+        stores[0][X].source = 1
+    return p, stores, X
 
 
-solver_types = [cmf.ExplicitEuler_fixed, cmf.BDF2, cmf.ImplicitEuler, cmf.RKFIntegrator, cmf.CVodeIntegrator]
+solver_types = [
+    cmf.ExplicitEuler_fixed, cmf.RKFIntegrator, cmf.HeunIntegrator,
+    cmf.BDF2,# cmf.ImplicitEuler,
+    cmf.CVodeIntegrator]
 
 
 class TestSolver(unittest.TestCase):
 
-    def test_solver_size(self):
-        p, stores = get_project()
+    def test_solver_size_solute(self):
+        p, stores, X = get_project(True)
         for st in solver_types:
             solver = st(p)
             self.assertEqual(len(solver), 20)
 
-    def test_solver_access(self):
-        p, stores = get_project()
+    def test_solver_size_nosolute(self):
+        p, stores, X = get_project()
+        for st in solver_types:
+            solver = st(p)
+            self.assertEqual(len(solver), 10)
+
+    def test_solver_access_solute(self):
+        p, stores, X = get_project(True)
         for st in solver_types:
             solver = st(p)
             self.assertIsInstance(solver[0], cmf.WaterStorage)
@@ -38,6 +53,56 @@ class TestSolver(unittest.TestCase):
             with self.assertRaises(IndexError):
                 _ = solver[len(solver)]
 
-# Check exactness
+    def test_solver_access_nosolute(self):
+        p, stores, X = get_project()
+        for st in solver_types:
+            solver = st(p)
+            self.assertIsInstance(solver[0], cmf.WaterStorage)
+            self.assertIsInstance(solver[-1], cmf.WaterStorage)
+            with self.assertRaises(IndexError):
+                _ = solver[len(solver)]
+
+    def test_solver_results_solute(self):
+
+        vol_ref = [0.04978707, 0.1493612, 0.2240418, 0.2240418, 0.1680314,
+                   0.1008188, 0.05040941, 0.02160403, 0.008101512, 0.003802992]
+        smass_ref = [0.9502129, 0.8008517, 0.5768099, 0.3527681, 0.1847368,
+                     0.08391794, 0.03350854, 0.0119045, 0.003802992, 0.001486583]
+
+        for st in solver_types:
+
+            p, stores, X = get_project(True)
+
+            solver = st(p)
+            solver.integrate_until(cmf.day * 3, cmf.h)
+
+            vol = [s.volume for s in stores]
+            smass = [s[X].state for s in stores]
+
+            rmse_v = sum((vr - v)**2 for v, vr in zip(vol, vol_ref)) / len(vol)
+            rmse_s = sum((sr - s)**2 for s, sr in zip(smass, smass_ref)) / len(smass)
+
+            self.assertAlmostEqual(1 - rmse_v, 1, 2, "RMSE between reference volume and {} too large".format(st.__name__))
+            self.assertAlmostEqual(1 - rmse_s, 1, 2, "RMSE between reference solute and {} too large".format(st.__name__))
+
+    def test_solver_results_nosolute(self):
+
+        vol_ref = [0.04978707, 0.1493612, 0.2240418, 0.2240418, 0.1680314,
+                   0.1008188, 0.05040941, 0.02160403, 0.008101512, 0.003802992]
+
+        for st in solver_types:
+
+            p, stores, X = get_project(False)
+
+            solver = st(p)
+            solver.integrate_until(cmf.day * 3, cmf.h)
+
+            vol = [s.volume for s in stores]
+
+            rmse_v = sum((vr - v)**2 for v, vr in zip(vol, vol_ref)) / len(vol)
+
+            self.assertAlmostEqual(1 - rmse_v, 1, 2, "RMSE between reference volume and {} too large".format(st.__name__))
+
+# Check solver results
 # Check jacobian (CVodeDirect, CVodeKLU)
 
